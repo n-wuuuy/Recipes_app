c5bf158dca7093612cc441f5801620438441361e
n.wuuuy <n.duboisky@gmail.com>
2023-11-09 15:02:50 +0300

    Add admin panel for categories, products, recipes and cooking steps

---
+++ Recipes_app/recipes_app/recipes/admin.py
@@ -0,0 +1,47 @@
+from django.contrib import admin
+
+from recipes.models import *
+
+
+# Register your models here.
+
+class StepsInline(admin.TabularInline):
+    model = Cooking_steps
+    extra = 1
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'weight')
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('title', 'owner', 'time_create')
+    list_filter = ('cooking_time', 'time_create', 'portion')
+    search_fields = ('title', 'products__name', 'category__name', 'owner')
+    inlines = [StepsInline]
+    fieldsets = (
+        (None, {
+            'fields': (('title', 'owner', 'description'),)
+        }),
+        (None, {
+            'fields': (('cooking_time', 'portion'),)
+        }),
+        (None, {
+            'fields': (('products', 'category'),)
+        }),
+        (None, {
+            'fields': (('picture', 'video'),)
+        })
+        )
+
+
+@admin.register(Cooking_steps)
+class StepsAdmin(admin.ModelAdmin):
+    list_display = ('title', 'recipe')
