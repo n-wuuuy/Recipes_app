bbe0824dab742e9dfbb547ee81a9d1fc6c65c563
n.wuuuy <n.duboisky@gmail.com>
2023-11-10 11:09:30 +0300

    Add recipe serializer.

--- Recipes_app/recipes_app/recipes/serializers.py
+++ Recipes_app/recipes_app/recipes/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from recipes.models import Category, Product
+from recipes.models import Category, Product, Recipe
 
 
 class CategorySerializer(serializers.ModelSerializer):
@@ -12,4 +12,10 @@ class CategorySerializer(serializers.ModelSerializer):
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
+        fields = '__all__'
+
+
+class RecipeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Recipe
         fields = '__all__'
