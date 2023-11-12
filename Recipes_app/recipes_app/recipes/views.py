5a7679e98e661681f12773fa957c273fbbf51791
n.wuuuy <n.duboisky@gmail.com>
2023-11-12 17:08:20 +0300

    Add view for product model.

--- Recipes_app/recipes_app/recipes/views.py
+++ Recipes_app/recipes_app/recipes/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.generics import ListAPIView
 from rest_framework.permissions import IsAdminUser
 
-from recipes.models import Category
-from recipes.serializers import CategorySerializer
+from recipes.models import Category, Product
+from recipes.serializers import CategorySerializer, ProductSerializer
 
 
 # Create your views here.
@@ -11,4 +11,9 @@ from recipes.serializers import CategorySerializer
 class CategoryModelView(ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
-    permission_classes = (IsAdminUser, )
+    permission_classes = (IsAdminUser,)
+
+
+class ProductModelView(ListAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
