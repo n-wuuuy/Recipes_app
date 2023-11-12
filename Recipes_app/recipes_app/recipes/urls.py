9e25a3d449c817077a864517b4922661be149ad9
n.wuuuy <n.duboisky@gmail.com>
2023-11-12 17:09:01 +0300

    Add url for product model and refactor url.

--- Recipes_app/recipes_app/recipes/urls.py
+++ Recipes_app/recipes_app/recipes/urls.py
@@ -1,2 +1,13 @@
+from django.urls import path
+from rest_framework.routers import SimpleRouter
+
+from recipes.views import CategoryModelView, ProductModelView
+
+router = SimpleRouter()
+router.register('api/v1/category', CategoryModelView)
+
 urlpatterns = [
+    path('api/v1/product', ProductModelView.as_view())
 ]
+
+urlpatterns += router.urls
