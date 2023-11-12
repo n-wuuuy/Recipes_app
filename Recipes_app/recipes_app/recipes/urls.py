87eda6a4e1894c6e66194e6907bd7350154a69d8
n.wuuuy <n.duboisky@gmail.com>
2023-11-12 18:16:11 +0300

    Сreated an url for the culinary steps.

--- Recipes_app/recipes_app/recipes/urls.py
+++ Recipes_app/recipes_app/recipes/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from rest_framework.routers import SimpleRouter
 
-from recipes.views import CategoryModelView, ProductListModelView
+from recipes.views import CategoryModelView, ProductModelView, CookingStepsModelView
 
 router = SimpleRouter()
 router.register('api/v1/category', CategoryModelView)
-router.register('api/v1/product', ProductListModelView)
+router.register('api/v1/product', ProductModelView)
+router.register('api/v1/steps', CookingStepsModelView)
 
 urlpatterns = [
 ]
