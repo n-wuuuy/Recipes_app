74b0a6e28b83fc1d803f485e72247cfbf3ecf9f3
n.wuuuy <n.duboisky@gmail.com>
2023-11-12 18:15:48 +0300

    Сreated a representation for the culinary steps model.

--- Recipes_app/recipes_app/recipes/views.py
+++ Recipes_app/recipes_app/recipes/views.py
@@ -3,8 +3,8 @@ from rest_framework.viewsets import ModelViewSet
 from rest_framework.generics import ListAPIView, GenericAPIView, CreateAPIView
 from rest_framework.permissions import IsAdminUser, AllowAny
 
-from recipes.models import Category, Product
-from recipes.serializers import CategorySerializer, ProductSerializer
+from recipes.models import Category, Product, CookingSteps
+from recipes.serializers import CategorySerializer, ProductSerializer, CookingStepsSerializer
 
 
 # Create your views here.
@@ -15,9 +15,21 @@ class CategoryModelView(ModelViewSet):
     permission_classes = (IsAdminUser,)
 
 
-class ProductListModelView(ModelViewSet):
+class ProductModelView(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+    def get_permissions(self):
+        if self.action in ['create', 'update', 'partial_update', 'destroy']:
+            self.permission_classes = [IsAdminUser, ]
+        elif self.action in ['list', 'retrieve']:
+            self.permission_classes = [AllowAny, ]
+        return super().get_permissions()
+
+
+class CookingStepsModelView(ModelViewSet):
+    queryset = CookingSteps.objects.all()
+    serializer_class = CookingStepsSerializer
 
     def get_permissions(self):
         if self.action in ['create', 'update', 'partial_update', 'destroy']:
