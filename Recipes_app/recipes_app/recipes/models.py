3a93434fb0a62580333495d4a81e102a680d3693
n.wuuuy <n.duboisky@gmail.com>
2023-11-12 16:50:56 +0300

    Add view set from category model.

--- Recipes_app/recipes_app/recipes/models.py
+++ Recipes_app/recipes_app/recipes/models.py
@@ -5,7 +5,7 @@ from django.db import models
 # Create your models here.
 
 class Category(models.Model):
-    name = models.CharField(max_length=256)
+    name = models.CharField(max_length=256, unique=True)
 
     def __str__(self):
         return f'{self.name}'
--- Recipes_app/recipes_app/recipes/views.py
+++ Recipes_app/recipes_app/recipes/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAdminUser
+
+from recipes.models import Category
+from recipes.serializers import CategorySerializer
+
 
 # Create your views here.
+
+class CategoryModelView(ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = (IsAdminUser, )
