8a4e2aa1a4c32850088aace7d7e849cb1f3f99d6
n.wuuuy <n.duboisky@gmail.com>
2023-11-18 18:45:27 +0300

    Create test for cooking steps serializer.

--- Recipes_app/recipes_app/recipes/tests/test_serializers.py
+++ Recipes_app/recipes_app/recipes/tests/test_serializers.py
@@ -1,7 +1,12 @@
+from datetime import datetime
+
+from django.contrib.auth.models import User
+from django.db.models import Count
 from django.test import TestCase
 
-from recipes.models import Category, Product
-from recipes.serializers import CategorySerializer, ProductSerializer
+from recipes.models import Category, Product, CookingSteps, Recipe
+from recipes.serializers import CategorySerializer, ProductSerializer, CookingStepsSerializer, RecipeDitailSerializer, \
+    RecipeListSerializer
 
 
 class CategorySerializerTestCase(TestCase):
@@ -39,6 +44,54 @@ class ProductSerializerTestCase(TestCase):
                 'id': product2.id,
                 'name': 'Patato',
                 'weight': 500
+            }
+        ]
+        self.assertEqual(exected_data, data)
+
+
+class CookingStepsSerializerTestCase(TestCase):
+    def test_serializer(self):
+        user1 = User.objects.create(username='user1',
+                                    first_name='Ivan',
+                                    last_name='Danilevich')
+        product1 = Product.objects.create(name='Cucumber', weight=250)
+        product2 = Product.objects.create(name='Patato', weight=450)
+        category1 = Category.objects.create(name='Soup')
+        recipe1 = Recipe.objects.create(title='broth',
+                                        description='tasty soup',
+                                        picture='',
+                                        cooking_time='00:30:00',
+                                        time_create=datetime.now(),
+                                        category=category1,
+                                        owner=user1,
+                                        portion=2,
+                                        video='https://www.youtube.com/watch?v=Z2NHP2NQD4k')
+        recipe1.save()
+        recipe1.products.add(product1, product2)
+        step1 = CookingSteps.objects.create(title='Cooking soup',
+                                            instruction='120',
+                                            picture='',
+                                            recipe=recipe1)
+        step2 = CookingSteps.objects.create(title='Wait',
+                                            instruction='Wait about 1:00:00',
+                                            picture='',
+                                            recipe=recipe1)
+        steps = CookingSteps.objects.all()
+        data = CookingStepsSerializer(steps, many=True).data
+        exected_data = [
+            {
+                'id': step1.id,
+                'title': 'Cooking soup',
+                'instruction': '120',
+                'picture': None,
+                'recipe': recipe1.id
+            },
+            {
+                'id': step2.id,
+                'title': 'Wait',
+                'instruction': 'Wait about 1:00:00',
+                'picture': None,
+                'recipe': recipe1.id
             }
         ]
         self.assertEqual(exected_data, data)
