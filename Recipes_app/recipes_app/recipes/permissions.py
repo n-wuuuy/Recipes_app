0836d6f44f7f684588355fe31f4c1c721453fa84
n.wuuuy <n.duboisky@gmail.com>
2023-11-20 15:12:27 +0300

    Create permission for admin or read only.

--- Recipes_app/recipes_app/recipes/permissions.py
+++ Recipes_app/recipes_app/recipes/permissions.py
@@ -8,3 +8,11 @@ class IsOwnerOrStaffOrReadOnly(BasePermission):
             request.user and
             request.user.is_authenticated and (obj.owner == request.user or request.user.is_staff)
         )
+
+
+class IsAdminOrReadOnly(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return bool(
+            request.method in SAFE_METHODS or
+            request.user and request.user.is_staff
+        )
